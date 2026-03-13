6c934eb752af4d8a327bd86a031058e1d74b57aa
Ubuntu <ubuntu@ip-172-31-13-91.ap-south-1.compute.internal>
2026-03-13 17:44:11 +0000

    Fix frontend URL update script and resolve Jenkinsfile merge conflict

--- Automations/updatebackendnew.sh
+++ Automations/updatebackendnew.sh
@@ -10,7 +10,7 @@ ipv4_address=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'R
 file_to_find="../backend/.env.docker"
 
 # Check the current FRONTEND_URL in the .env file
-current_url=$(sed -n "/lp" $file_to_find)
+current_url=$(grep FRONTEND_URL $file_to_find)
 
 # Update the .env file if the IP address has changed
 if [[ "$current_url" != "FRONTEND_URL=\"http://${ipv4_address}:5173\"" ]]; then
